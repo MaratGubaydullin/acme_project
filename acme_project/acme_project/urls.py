888874bf621e2d1d4b76f0994af9ccabf797306a
mgubaydullin <mgubaydullin@131.ru>
2024-10-01 19:10:50 +0300

    main fix
    FIX

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -8,8 +8,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, reverse_lazy
 
-from django.conf import settings
-
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
